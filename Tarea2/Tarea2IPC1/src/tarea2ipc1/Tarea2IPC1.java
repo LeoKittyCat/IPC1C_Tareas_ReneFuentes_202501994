6d9a56d356912a6168d845dcfe4673a40b51a6db
Leo_Just_a_Cat <hg2018rene@gmail.com>
2026-08-05 22:18:59 -0600

    agregar estructura inicial y tipos de datos

---
+++ Tarea2/Tarea2IPC1/src/tarea2ipc1/Tarea2IPC1.java
@@ -0,0 +1,39 @@
+package tarea2ipc1;
+
+//Imports. Importamos Scanner para que el usuario pueda escribir
+
+import java.util.Scanner;
+
+//Este es mi main, solo que tiene nombre de la tarea 
+public class Tarea2IPC1 {
+
+    static Scanner scanner = new Scanner(System.in); //Se usa static para que sea accesible incluso fuera de main
+    
+    public static void main(String[] args) {
+        // Prueba para ver que si se guarden los cambios en github: System.out.println("Hola mundo");
+        
+        //Variables con diferentes tipos de datos
+        int cantidadNumeros = 5; //Guarda numeros enteros(sin decimales)
+        double promedioInicial = 0.0; //Guarda numeros con punto decimal
+        char opcionContinuar = 'S'; //Guarda una sola letrd o caracter (siempre con comillas simples, o almenos creo '')
+        boolean programaActivo = true; //Guarda un valor logico: verdadero o falso
+        String nombrePrograma = "Analizador de numeros y matrices"; //Guarda texto completo
+        
+        /*
+        * Este programa solicita numeros al usuario
+        * Validamos los datos ingresados
+        * Utilizamos los arreglos que nos pide el proyecto
+        */
+        
+        System.out.println("================================="); //Esto es solo para imprimir el titulo de una forma fancy jeje
+        System.out.println(nombrePrograma);
+        System.out.println("=================================");
+        
+        System.out.println("Cantidad de numeros: " + cantidadNumeros);
+        System.out.println("Promedio inicial: " + promedioInicial);
+        System.out.println("Opcion para continuar: " + opcionContinuar);
+        System.out.println("Programa activo: " + programaActivo);
+        
+    }
+    
+}
