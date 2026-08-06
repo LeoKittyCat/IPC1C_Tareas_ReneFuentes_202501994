577829d93e0c27b1e6d6adf2c4310811f2225302
Leo_Just_a_Cat <hg2018rene@gmail.com>
2026-08-06 15:17:55 -0600

    Agregar arreglo unidimensional y calculos

--- Tarea2/Tarea2IPC1/src/tarea2ipc1/Tarea2IPC1.java
+++ Tarea2/Tarea2IPC1/src/tarea2ipc1/Tarea2IPC1.java
@@ -13,7 +13,6 @@ public class Tarea2IPC1 {
         // Prueba para ver que si se guarden los cambios en github: System.out.println("Hola mundo");
         
         //Variables con diferentes tipos de datos
-        int cantidadNumeros = 5; //Guarda numeros enteros(sin decimales)
         double promedioInicial = 0.0; //Guarda numeros con punto decimal
         char opcionContinuar = 'S'; //Guarda una sola letrd o caracter (siempre con comillas simples, o almenos creo '')
         boolean programaActivo = true; //Guarda un valor logico: verdadero o falso
@@ -29,13 +28,21 @@ public class Tarea2IPC1 {
         System.out.println(nombrePrograma);
         System.out.println("=================================");
         
-        System.out.println("Cantidad de numeros: " + cantidadNumeros);
         System.out.println("Promedio inicial: " + promedioInicial);
         System.out.println("Opcion para continuar: " + opcionContinuar);
         System.out.println("Programa activo: " + programaActivo);
+        
+        int cantidadNumeros = leerEnteroValidado( /*Leemos cuantos datos va a ingresar el usuario*/
+        "Cuantos numeros desea ingresar? (entre 1 y 10): ",
+                1,
+                10);
+        int[] numeros = new int[cantidadNumeros]; /*Guardamos los datos*/
+        
+        llenarArreglo(numeros);
+       mostrarResultadosArreglos(numeros);
     }
     
-    public static int leerEneteroValidado(String mensaje, int minimo, int maximo) {
+    public static int leerEnteroValidado(String mensaje, int minimo, int maximo) {
         int numero; //Con esta variable puedo guardar el numero que ingrese el usuario, el cual debe de ser entero
         
         while (true) {
@@ -52,7 +59,7 @@ public class Tarea2IPC1 {
                 
                  System.out.println(
                  "Error: el numero debe estar entre "
-                 + minimo + "y" + maximo + "."); //Mostrar mensaje de error si el usuario ingresa un valor invalido
+                 + minimo + " y " + maximo + "."); //Mostrar mensaje de error si el usuario ingresa un valor invalido
                 
                 
             } else {
@@ -62,4 +69,42 @@ public class Tarea2IPC1 {
         }
     }
     
+    public static void llenarArreglo(int[] numeros){
+        
+        System.out.println("\n--- INGRESO DE NUMEROS ---");
+        
+        for (int i = 0; i < numeros.length; i++){
+            
+            numeros[i] = leerEnteroValidado(
+            "Ingrese el numero " + (i + 1) //Ponemos el +1 para que el mensaje sea "ingrese numero 1" en vez de "ingrese numero 0"
+            + " entre 1 y 100: ",
+                    1,
+                    100);
+        }
+    }
+    
+    public static void mostrarResultadosArreglos(int[] numeros) {
+        
+        int maximo = numeros[0]; //Tomamos el primer numero del arreglo como punto de partida
+        int minimo = numeros[0];
+        int suma = 0; //Comienza desde 0
+        
+        for (int i = 0; i < numeros.length; i++) {
+            
+            suma = suma + numeros[i]; //Compararemos los numeros desde el inicio
+            
+            if (numeros[i] > maximo) { //Comparamos si el numero actual es mayor que el maximo numero ingresado
+                maximo = numeros[i];
+            }
+            if (numeros[i] < minimo){ //Lo mismo que el mayor, pero para el menor XD
+                minimo = numeros[i];
+        }
+    }
+        double promedio = (double) suma / numeros.length; //Divide la suma entre la cantidad de numeros
+        
+        System.out.println("\n--- RESULTADOS DEL ARREGLO ---");
+        System.out.println("Valor maximo: " + maximo);
+        System.out.println("Valor minimo: " + minimo);
+        System.out.println("Promedio: " + promedio);
+    }
 }
